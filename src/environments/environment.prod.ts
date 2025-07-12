d099df69c3de73d41436e4fdb54493139b0ce558
PlinioM <mcplinio.15@gmail.com>
2025-07-12 17:17:27 -0500

    Prueba de CI/CD con Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
 // https://registro-escolar-backen.onrender.com/swagger-ui/index.html#/persona-controller/listar_1
 
   production: true,
-  apiUrl: 'https://registro-escolar-backen.onrender.com/api'
+  apiUrl: 'https://miapp-1002497481751.us-central1.run.app/api'
 };
